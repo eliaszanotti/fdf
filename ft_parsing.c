f3474d002f9476f00b9e435db0acdf74cf057efc
Elias Zanotti <ezanotti@z2r11p6.42lyon.fr>
2022-11-25 18:42:09 +0100

    add parsing function : (issue : leaks)

--- ft_parsing.c
+++ ft_parsing.c
@@ -6,17 +6,15 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:00:27 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/25 18:15:59 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/25 18:42:00 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include <stdio.h>
-
-int		*ft_get_atoi_tab(char **tab)
+int	*ft_get_atoi_tab(char **tab)
 {
-	int		*int_tab; 
+	int		*int_tab;
 	ssize_t	i;
 
 	i = 0;
@@ -32,36 +30,47 @@ int		*ft_get_atoi_tab(char **tab)
 	return (int_tab);
 }
 
+int	**ft_get_int_tab(t_list *first)
+{
+	int	**int_tab;
+	int	size;
+
+	size = ft_lstsize(first);
+	int_tab = ft_calloc(size + 1, sizeof(int *));
+	size = 0;
+	while (first)
+	{
+		int_tab[size++] = first->content;
+		first = first->next;
+	}
+	while (first)
+	{
+		free(first->content);
+		first = first->next;
+	}
+	free(first);
+	return (int_tab);
+}
+
 int	**ft_parsing(int fd)
 {
 	t_list	*first;
 	t_list	*new;
 	char	*line;
 	int		*int_tab;
-	size_t	size;
-	int		**new_tab;
-	
+
 	first = NULL;
 	line = ft_calloc(1, sizeof(char));
 	while (1)
 	{
 		line = get_next_line(fd);
 		if (!line)
-			break;
+			break ;
 		int_tab = ft_get_atoi_tab(ft_split(line, ' '));
 		free(line);
 		new = ft_lstnew(int_tab);
 		ft_lstadd_back(&first, new);
-		//free(int_tab);
 	}
-	size = ft_lstsize(first);
-	new_tab = ft_calloc(size + 1, sizeof(int *));
-	size = 0;
-	while (first)
-	{
-		new_tab[size++] = first->content;
-		first = first->next;
-	}
-	free(first);
-	return (new_tab);
+	free(new);
+	return (ft_get_int_tab(first));
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 11:37:49 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/25 17:25:02 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/25 18:22:14 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
-t_list	*ft_lstnew(void *content);
+t_list	*ft_lstnew(int *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
