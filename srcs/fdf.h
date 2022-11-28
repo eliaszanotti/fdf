d83bf9c6d4cdf3cf9065eb3f37c57297fcd045b4
Elias Zanotti <ezanotti@z2r10p8.42lyon.fr>
2022-11-28 18:51:34 +0100

    nrm

--- srcs/fdf.h
+++ srcs/fdf.h
@@ -6,7 +6,7 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:07:28 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/28 17:59:26 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/28 18:51:11 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,14 @@ typedef struct s_line
 	int	x2;
 	int	y2;
 }	t_line;
+
+typedef struct s_offset
+{
+	int	left;
+	int	top;
+	int	left_offset;
+	int	top_offset;
+}	t_offset;
 
 int		**ft_parsing(int fd, int *lines, int *cols);
 void	ft_display_grid(int **tab, t_vars window);
--- srcs/ft_display_grid.c
+++ srcs/ft_display_grid.c
@@ -6,7 +6,7 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:39:10 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/28 18:37:28 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/28 18:50:51 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,40 @@
 #define MAX 7
 #define MIN 4
 
-#include <stdio.h>
+void	ft_place_line(t_vars window, t_offset offset, int i, int j)
+{
+	t_line	line;
+
+	line.x1 = (j - i) * MAX + offset.left + offset.left_offset;
+	line.y1 = (i + j) * MIN + offset.top_offset;
+	line.x2 = line.x1 + MAX;
+	line.y2 = line.y1 + MIN;
+	if (j != window.cols - 1)
+		ft_print_line(window, line);
+	line.x2 = line.x2 - (MAX * 2);
+	if (i != window.lines - 1)
+		ft_print_line(window, line);
+	//mlx_string_put(window.mlx, window.win, line.x1, line.y1, 0255000000, ft_itoa(line.y1));
+}
 
 void	ft_display_grid(int **tab, t_vars window)
 {
-	t_line	line;
-	int left = (window.lines - 1) * MAX;	
-	int top = (window.lines + window.cols - 2) * MIN;
-	int left_offset = (1920 - ((window.lines + window.cols) * MAX)) / 2;
-	int top_offset = (1080 - top) / 2;
-	int i = 0;
-	int j = 0;
+	t_offset	offset;
+	int			i;
+	int			j;
 
+	offset.left = (window.lines - 1) * MAX;
+	offset.top = (window.lines + window.cols - 2) * MIN;
+	offset.left_offset = (1920 - ((window.lines + window.cols) * MAX)) / 2;
+	offset.top_offset = (1080 - offset.top) / 2;
 	window.mlx = mlx_init();
 	window.win = mlx_new_window(window.mlx, 1920, 1080, "Fil De Fer");
+	i = 0;
 	while (i < window.lines)
 	{
 		j = 0;
 		while (j < window.cols)
-		{
-			line.x1 = (j - i) * MAX + left + left_offset;
-			line.y1 = (i + j) * MIN + top_offset;
-			line.x2 = line.x1 + MAX;
-			line.y2 = line.y1 + MIN;
-			if (j != window.cols - 1)
-				ft_print_line(window, line);
-			line.x2 = line.x2 - (MAX * 2);
-			if (i != window.lines - 1)
-				ft_print_line(window, line);
-			//mlx_string_put(window.mlx, window.win, line.x1, line.y1, 0255000000, ft_itoa(line.y1));
-			j++;
-		}
+			ft_place_line(window, offset, i, j++);
 		i++;
 	}
 	//mlx_destroy_window(mlx, mlx_win);
