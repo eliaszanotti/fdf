5c91aa5a5a07fb5ed9ce442182bbbba8dbd541a0
Elias Zanotti <ezanotti@z2r11p6.42lyon.fr>
2022-12-01 11:10:51 +0100

    add altitude modifier

--- srcs/ft_display_map.c
+++ srcs/ft_display_map.c
@@ -6,7 +6,7 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:39:10 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/30 15:52:13 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/12/01 11:10:27 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,10 @@
 int	ft_hook_key(int key, t_grid *grid)
 {
 	printf("%d\n", key);
-	if (key == 126 && grid->rotation < 1)
-	{
-		grid->rotation += 0.1;
-		grid->min = grid->rotation * grid->max;
-	}
-	if (key == 125 && grid->rotation > 0)
-	{
-		grid->rotation -= 0.1;
-		grid->min = grid->rotation * grid->max;
-	}
+	if (key == 126)
+		grid->altitude += 0.25;
+	if (key == 125 && grid->altitude >= 0.25)
+		grid->altitude -= 0.25;
 
 
 	mlx_destroy_image(grid->mlx, grid->image.img);
@@ -68,26 +62,11 @@ void	ft_update_map(t_grid *grid)
 	mlx_put_image_to_window(grid->mlx, grid->win, grid->image.img, 0, 0);
 }
 
-int ft_close(int key, int x, int y, t_grid *grid)
-{
-	if (key == 1)
-		grid->rotation = 1;
-	if (key == 2)
-		grid->rotation = 0;
-	mlx_destroy_image(grid->mlx, grid->image.img);
-	ft_update_map(grid);
-	printf("%d,%d\n", x, y);
-	(void)x;
-	(void)y;
-	(void)grid;
-	return (0);
-}
-
 void	ft_create_map(t_grid *grid)
 {
 
 	ft_update_map(grid);
 	//mlx_mouse_hook(grid->win, mouse_hook, grid);
-	mlx_hook(grid->win, 5, 0, ft_close, grid);	
+	//mlx_hook(grid->win, 5, 0, ft_close, grid);	
 	mlx_key_hook(grid->win, ft_hook_key, grid);
 }
