4b4259e6b4a26dc935662ace9e074928fae221ee
Elias Zanotti <ezanotti@z2r10p8.42lyon.fr>
2022-12-01 15:18:02 +0100

    add all move : (working)

--- srcs/ft_hooks.c
+++ srcs/ft_hooks.c
@@ -6,42 +6,56 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 14:28:36 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/12/01 14:33:05 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/12/01 15:14:56 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-
-#include <stdio.h>
-
 int	ft_key_hook(int key, t_grid *grid)
 {
+	static int	move;
 
-	printf("%d\n", key);
-
-	//mlx_destroy_image(grid->mlx, grid->image.img);
-	//ft_update_map(grid);
-	(void)grid;
+	move = 250;
+	if (key == 126)	
+		grid->off_top += move;
+	if (key == 125)	
+		grid->off_top -= move;
+	if (key == 123)	
+		grid->off_left += move;
+	if (key == 124)	
+		grid->off_left -= move;
+	if (key == 69)	
+		grid->altitude += 0.1;
+	if (key == 78 && grid->altitude > 0.1)	
+		grid->altitude -= 0.1;
+	mlx_destroy_image(grid->mlx, grid->image.img);
+	ft_update_map(grid);
 	return (0);
 }
 
 int ft_mouse_hook(int key, int x, int y, t_grid *grid)
 {
-	x = 0;
-	y = 0;
 	if (key == 5)
+	{
 		grid->max += 1;
+		if (grid->altitude > 0.1)
+			grid->altitude += 0.1;
+	}
 	if (key == 4 && grid->max > 1)
+	{
 		grid->max -= 1;
-
-	/*grid->left = (grid->lines - 1) * grid->max;
-	grid->top = (grid->lines + grid->cols - 2) * grid->min;
-	grid->off_left = grid->left + \
-		(WIN_W - ((grid->lines + grid->cols) * grid->max)) / 2;
-	grid->off_top = (WIN_H - grid->top) / 2;*/
-
+		if (grid->altitude >= 0.1)
+			grid->altitude -= 0.1;
+	}
+	grid->off_left = (WIN_W - ((grid->lines + grid->cols) * grid->max) / \
+		sqrt(2)) / 2;
+	grid->off_left += (grid->lines * grid->max / sqrt(2));
+	grid->off_top = (WIN_H - (grid->max * (grid->lines + grid->cols)) / \
+		sqrt(6)) / 2;
 	mlx_destroy_image(grid->mlx, grid->image.img);
 	ft_update_map(grid);
+	x = 0;
+	y = 0;
 	return (0);
 }
