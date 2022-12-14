c5bcf7ef6d7b9d126d91dbd8aa31091e7736bbf8
Event Event <event01@z2r12p2.42lyon.fr>
2022-12-14 12:49:45 +0100

    nrm

--- srcs/ft_struct_init.c
+++ srcs/ft_struct_init.c
@@ -6,7 +6,7 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 11:13:58 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/12/12 16:48:24 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/12/14 12:49:20 by event01          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ t_grid	*ft_struct_init(int fd)
 	if (grid->lines + grid->cols == 2)
 		grid->max = 1;
 	else
-		grid->max = round(((WIN_W + WIN_H) / 2) / (grid->lines + grid->cols - 2));
+		grid->max = round(((WIN_W + WIN_H) / 2) \
+				/ (grid->lines + grid->cols - 2));
 	grid->off_left = (WIN_W - ((grid->lines + grid->cols) * grid->max) / \
 		sqrt(2)) / 2;
 	grid->off_left += (grid->lines * grid->max / sqrt(2));
